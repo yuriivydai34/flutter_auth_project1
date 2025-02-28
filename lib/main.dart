7a4c28dceffe3c1ca64b77b6a00af6da6b7bc52e
Yurii Vydai <djurker21@gmail.com>
2025-02-28 16:02:34 +0200

    screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 // Path: `./lib/main.dart`
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:flutter_auth_project1/screens/article_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
@@ -16,70 +15,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class ArticleScreen extends StatefulWidget {
-  @override
-  _ArticleScreenState createState() => _ArticleScreenState();
-}
-
-class _ArticleScreenState extends State<ArticleScreen> {
-  List articles = [];
-
-  @override
-  void initState() {
-    super.initState();
-    fetchArticles();
-  }
-
-  Future<void> fetchArticles() async {
-    final response = await http.get(
-      Uri.parse('${dotenv.get('baseUrl')}/articles?populate=*'),
-    );
-    if (response.statusCode == 200) {
-      print('Raw API Response: ${jsonEncode(response.body)}');
-      setState(() {
-        articles = jsonDecode(response.body)['data'];
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Strapi + Flutter')),
-      body: ListView.builder(
-        itemCount: articles.length,
-        itemBuilder: (context, index) {
-          var article = articles[index];
-          debugPrint(article.toString());
-          return Card(
-            margin: EdgeInsets.all(10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // if (article['image'] != null)
-                Image.network('${article['image']}'),
-                Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        article['title'],
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 5),
-                      Text(article['description']),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
